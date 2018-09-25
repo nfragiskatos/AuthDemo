ef60119b5f51d79bba9f8c42d37d889f8cbf2003
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-25 03:29:02 +0000

    implemented the sign up functionality

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ mongoose.connect("mongodb://localhost:27017/auth_demo_app", {useNewUrlParser: tr
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(require("express-session")({
     secret: "Yogi is the weirdest and craziest dog everrrr",
     resave: false,
@@ -24,6 +26,10 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//=============================================================
+// ROUTES
+//=============================================================
+
 app.get("/", function(req, res) {
     res.render("home"); 
 });
@@ -31,6 +37,31 @@ app.get("/", function(req, res) {
 app.get("/secret", function(req, res) {
     res.render("secret"); 
 });
+
+// Auth route - show signup form
+app.get("/register", function(req, res) {
+    res.render("register");  
+});
+
+// Auth route - handling user signup
+app.post("/register", function(req, res) {
+    req.body.username;
+    req.body.password;
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/secret");    
+        });
+    });
+});
+
+
+
+
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("server started..."); 
